eca0b088e3b42adb4453263a824d76ed21ff682e
Bernhard Gebetsberger <b.gebetsberger@posteo.at>
2024-12-03 21:58:33 +0100

    Add solution for day3

---
+++ day3/src/main.rs
@@ -0,0 +1,33 @@
+use std::fs::read_to_string;
+use std::error::Error;
+use regex::Regex;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let content = read_to_string("input.txt")?;
+
+    let mut sum_part1 = 0;
+    let mut sum_part2 = 0;
+
+    let mut count_enabled = true;
+    let re = Regex::new(r"(do\(\))|(don't\(\))|mul\(([0-9]{1,3}),([0-9]{1,3})\)")?;
+    for captures in re.captures_iter(&content) {
+        if let Some(_) = captures.get(1) {
+            count_enabled = true;
+        } else if let Some(_) = captures.get(2) {
+            count_enabled = false;
+        } else {
+            let nbr1 = captures.get(3).unwrap().as_str().parse::<i32>()?;
+            let nbr2 = captures.get(4).unwrap().as_str().parse::<i32>()?;
+            let result = nbr1 * nbr2;
+
+            sum_part1 += result;
+            if count_enabled {
+                sum_part2 += result;
+            }
+        }
+    }
+
+    println!("Part 1: {sum_part1}");
+    println!("Part 2: {sum_part2}");
+    Ok(())
+}
