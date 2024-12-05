85acbc8ec8fdce0df68787bf8faa604d66c060b9
Bernhard Gebetsberger <b.gebetsberger@posteo.at>
2024-12-05 21:28:55 +0100

    Iterate over mutable references instead

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -40,15 +40,15 @@ fn main() -> Result<(), io::Error> {
     let lines = read_to_string("input.txt")?.get_lines();
 
     let rules = lines.numbers_in_line(b'|');
-    let items = lines.numbers_in_line(b',');
+    let mut items = lines.numbers_in_line(b',');
 
     let mut sum = 0;
     let mut sum_part_2 = 0;
-    for mut item in items {
+    for item in &mut items {
         if check_valid(&rules, &item) {
             sum += item[item.len() / 2];
         } else {
-            while !check_valid_swap(&rules, &mut item) {}
+            while !check_valid_swap(&rules, item) {}
             sum_part_2 += item[item.len() / 2];
         }
     }
