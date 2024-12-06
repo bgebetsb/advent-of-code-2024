a8f4143a5af9f88540eb276fe6b00d404f181a19
Bernhard Gebetsberger <b.gebetsberger@posteo.at>
2024-12-06 14:11:57 +0100

    better solution for day6

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,11 +1,40 @@
-use utils::*;
 use std::fs::read_to_string;
 use std::io;
+use utils::*;
 
-fn get_start_pos(map: &Vec<Vec<u8>>) -> Option<(usize, usize)> {
+#[derive(PartialEq, Clone, Debug)]
+enum Directions {
+    North,
+    South,
+    East,
+    West,
+}
+
+#[derive(Clone, Debug)]
+struct MapField {
+    field_type: u8,
+    visited: Vec<Directions>,
+}
+
+impl MapField {
+    fn new(field_type: u8) -> Self {
+        MapField {
+            field_type: field_type,
+            visited: Vec::new(),
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+enum MapResult {
+    Infinite,
+    Finished(usize),
+}
+
+fn get_start_pos(map: &Vec<Vec<MapField>>) -> Option<(usize, usize)> {
     for i in 0..map.len() {
         for j in 0..map[i].len() {
-            if map[i][j] == b'^' {
+            if map[i][j].field_type == b'^' {
                 return Some((i, j));
             }
         }
@@ -13,179 +42,95 @@ fn get_start_pos(map: &Vec<Vec<u8>>) -> Option<(usize, usize)> {
     None
 }
 
-#[derive(PartialEq, Clone, Debug)]
-enum Directions {
-    North,
-    South,
-    East,
-    West
-}
-
-fn step(map: &mut Vec<Vec<u8>>, direction: &mut Directions, y: &mut usize, x: &mut usize) -> bool {
-    match *direction {
-        Directions::North => {
-            if *y == 0 {
-                false
-            } else if map[*y - 1][*x] == b'#' {
-                *direction = Directions::East;
-                false
-            } else {
-                *y -= 1;
-                map[*y][*x] = b'X';
-                true
-            }
-        },
-        Directions::East => {
-            if *x == map[*y].len() - 1 {
-                false
-            } else if map[*y][*x + 1] == b'#'{
-                *direction = Directions::South;
-                false
-            } else {
-                *x += 1;
-                map[*y][*x] = b'X';
-                true
-            }
-        },
-        Directions::South => {
-            if *y == map.len() - 1 {
-                false
-            } else if map[*y + 1][*x] == b'#' {
-                *direction = Directions::West;
-                false
-            } else {
-                *y += 1;
-                map[*y][*x] = b'X';
-                true
-            }
-        },
-        _ => {
-            if *x == 0 {
-                false
-            } else if map[*y][*x - 1] == b'#' {
-                *direction = Directions::North;
-                false
-            } else {
-                *x -= 1;
-                map[*y][*x] = b'X';
-                true
-            }
-        }
-    }
-}
-
-/*
-fn create_dirs_map(map: &Vec<Vec<u8>>) -> Vec<Vec<Vec<Directions>>> {
-    let mut dirs: Vec<Vec<Vec<Directions>>> = Vec::new();
-    for i in 0..map.len() {
-        dirs.push(Vec::new());
-        for _ in 0..map[i].len() {
-            dirs[i].push(Vec::new());
-        }
-    }
-    dirs
-}
-
-
-fn infinite_check(map: &Vec<Vec<u8>>, starty: usize, startx: usize) -> i32 {
-    let mut infinite = 0;
-
-     for i in 0..map.len() {
-        for j in 0..map[i].len() {
-            let mut currentmap = map.clone();
-            let mut direction = Directions::North;
-            if (i == starty && j == startx) || map[i][j] == b'#' {
-                continue;
-            }
-            let mut dirs = create_dirs_map(&currentmap);
-            let mut cur_x = startx;
-            let mut cur_y = starty;
-            let mut streak = 0;
-            loop {
-                if dirs[cur_y][cur_x].contains(&direction) {
-                    infinite += 1;
-                    println!("{:?}", dirs[cur_y][cur_x]);
-                    break;
-                }
-                
-                let prev_y = cur_y;
-                let prev_x = cur_x;
-                let prev_directon = direction.clone();
-                
-                if step(&mut currentmap, &mut direction, &mut cur_y, &mut cur_x) {
-                    dirs[prev_y][prev_x].push(prev_directon);
-                    if cur_y == 0 || cur_x == 0 || cur_y == currentmap.len() - 1 || cur_x == currentmap[cur_y].len() - 1 {
-                        break;
-                    }
-                } else if streak == 4 {
-                    break;
-                } else {
-                    streak += 1;
-                }
-            }
-        }
-    }
-    infinite
-}
-*/
-
-fn count_steps(map: &mut Vec<Vec<u8>>, starty: usize, startx: usize, stop_val: usize) -> usize {
-    let mut steps = 1;
-    let mut streak = 0;
+fn run_simulation(og_map: &Vec<Vec<MapField>>) -> MapResult {
+    let mut map = og_map.clone();
+    let (starty, startx) = get_start_pos(&map).expect("Unable to find start position");
     let mut direction = Directions::North;
 
-    let (mut y, mut x) = (starty, startx);
-
+    let mut y = starty;
+    let mut x = startx;
+    let mut fields_visited = 0;
     loop {
-        if step(map, &mut direction, &mut y, &mut x) {
-            steps += 1;
-            streak = 0;
-        } else if streak == 4 {
-            break;
-        } else {
-            streak += 1;
+        if map[y][x].visited.contains(&direction) {
+            return MapResult::Infinite;
         }
-        if steps > stop_val{
-            break;
+
+        if map[y][x].visited.is_empty() {
+            fields_visited += 1;
+        }
+
+        map[y][x].visited.push(direction.clone());
+        if y == 0 || x == 0 || y == map.len() - 1 || x == map[y].len() - 1 {
+            return MapResult::Finished(fields_visited);
+        }
+
+        match direction {
+            Directions::North => {
+                if map[y - 1][x].field_type == b'#' {
+                    direction = Directions::East;
+                } else {
+                    y -= 1;
+                }
+            }
+            Directions::East => {
+                if map[y][x + 1].field_type == b'#' {
+                    direction = Directions::South;
+                } else {
+                    x += 1;
+                }
+            }
+            Directions::South => {
+                if map[y + 1][x].field_type == b'#' {
+                    direction = Directions::West;
+                } else {
+                    y += 1;
+                }
+            }
+            _ => {
+                if map[y][x - 1].field_type == b'#' {
+                    direction = Directions::North;
+                } else {
+                    x -= 1;
+                }
+            }
         }
     }
-    steps
 }
 
-fn infinite_check(map: &Vec<Vec<u8>>, starty: usize, startx: usize) -> usize {
-    let mut infinite = 0;
+fn convert_input(input: &Vec<Vec<u8>>) -> Vec<Vec<MapField>> {
+    let mut mapfields = Vec::new();
+    for i in 0..input.len() {
+        mapfields.push(Vec::new());
+        for j in 0..input[i].len() {
+            mapfields[i].push(MapField::new(input[i][j]));
+        }
+    }
+    mapfields
+}
 
-    let original_steps = count_steps(&mut map.clone(), starty, startx, usize::MAX) + 278; // not proud of this
+fn main() -> Result<(), io::Error> {
+    let chars = read_to_string("input.txt")?.get_lines().lines_as_bytes();
+    let mut map = convert_input(&chars);
+
+    let result = run_simulation(&map);
+    if let MapResult::Finished(value) = result {
+        println!("Part 1: {}", value);
+    }
+    let mut infinite_count = 0;
     for i in 0..map.len() {
         for j in 0..map[i].len() {
-            if (i == starty && j == startx) || map[i][j] == b'#' {
-                continue;
-            }
-            let mut newmap = map.clone();
-            newmap[i][j] = b'#';
-            if count_steps(&mut newmap, starty, startx, original_steps) > original_steps {
-                infinite += 1;
+            if map[i][j].field_type == b'.' {
+                let tmp = map[i][j].field_type;
+                map[i][j].field_type = b'#';
+                if run_simulation(&map) == MapResult::Infinite {
+                    infinite_count += 1;
+                }
+                map[i][j].field_type = tmp;
             }
         }
     }
-    infinite
-}
 
-fn main() -> Result<(), io::Error>{
-    let mut chars = read_to_string("input.txt")?.get_lines().lines_as_bytes();
-    let (y, x) = get_start_pos(&chars).unwrap();
-
-    chars[y][x] = b'X';
-
-    let (starty, startx) = (y, x);
-    let steps = count_steps(&mut chars.clone(), y, x, usize::MAX);
-
-    println!("Part 1: {}", steps);
-
-  
-    let infinite = infinite_check(&chars, starty, startx);
-    
-    println!("Infinite: {}", infinite);
+    println!("Part 2: {}", infinite_count);
 
     Ok(())
 }
